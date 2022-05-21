bf9979089a537fa30a53a1e94b72c42415d5624e
alperinan <alperinan@hotmail.com>
2022-05-21 09:48:12 -0500

    final changes before transfer

--- Active_Directory_Utility/Program.cs
+++ Active_Directory_Utility/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using ActiveDirectoryUtilities;
+
+namespace Active_Directory_Utility
+{
+    class Program
+    {
+        //sample usage of functions
+
+        static void Main(string[] args)
+        {
+            //let's create the instance
+            AdData ADdata = new AdData();
+
+            //this function takes User_Id (string) as parameter and returns a boolean value showing user is active or not
+            bool IsUserActive = ADdata.IsUserActive("User_Id");
+
+            //this function takes User_Id (string) as parameter and returns an enum result showing user status: enabled, disabled or deleted
+            AccountStatus UserAccountStatus = ADdata.GetActiveDirectoryStatusBySamAccountName("User_Id");
+
+
+        }
+
+    }
+}
